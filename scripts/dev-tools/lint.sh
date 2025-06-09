80dcdc3c0c5a1bbc133cfea31c5c4c6ea8c95e97
kkonnectinc1 <kkonnectinc@gmail.com>
2025-06-09 00:19:44 +0000

    Cleaned up scripts, added test server and sample JS

--- scripts/dev-tools/lint.sh
+++ scripts/dev-tools/lint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 echo "🔍 Running lint..."
-
-if [ -f package.json ]; then
-  npx eslint . || true
-else
-  echo "No lint configured."
-fi
+eslint . || echo "No lintable files found or something went wrong."
